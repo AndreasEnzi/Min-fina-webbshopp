fb969b61b1047b151d3177ba21cf233070477b8c
Andreas <Andreas_93l@hotmail.com>
2020-10-13 15:44:14 +0200

    commit #3

--- script1.js
+++ script1.js
@@ -21,12 +21,12 @@ function addToCart1(){
     document.getElementById("korg").src="/images/rank2.jpg";
    }
    
-   else if(amount>=2){
+    if(amount>=2){
 
         if(coachingCounter>=1&&counter>=1)
    {
 
-    document.getElementById("garanterat").innerHTML=" Du har valt både coachning och boostning! Grandmaster är garanterat! grattis!";
+    document.getElementById("garanterat").innerHTML=" Du har valt både coachning och boostning! Grandmaster är garanterat! Grattis!";
    
     document.getElementById("korg").src="/images/GM1.jpg";
    }
@@ -36,9 +36,9 @@ function addToCart1(){
    
 }
 function addToCart2(){
-    amount ++;
+    amount++;
     coachingCounter++;
-    
+
     document.getElementById("coachingCounter").innerHTML= " Du har lagt till " + coachingCounter + " av den bästa coachningen ever! glhf!";
     alert('added to cart coaching'+ " "+amount);
   
@@ -53,11 +53,11 @@ function addToCart2(){
     document.getElementById("korg").src="/images/rank2.jpg";
    }
    
-   else if(amount>=2){
+    if(amount>=2){
     if(coachingCounter>=1&&counter>=1)
     {
  
-     document.getElementById("garanterat").innerHTML=" Du har valt både coachning och boostning! Grandmaster är garanterat! grattis!";
+     document.getElementById("garanterat").innerHTML=" Du har valt både coachning och boostning! Grandmaster är garanterat! Grattis!";
     
             document.getElementById("korg").src="/images/GM1.jpg";
     }
