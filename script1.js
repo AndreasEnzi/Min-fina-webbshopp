391ef2781a54a6d5e76b7806b1e7223df592e6f6
Andreas <Andreas_93l@hotmail.com>
2020-10-13 15:35:08 +0200

    Second commit

--- script1.js
+++ script1.js
@@ -1,9 +1,11 @@
 var amount =0;
 var counter=0;
 var coachingCounter=0;
-function addToCart1(){
-    counter++;
+function addToCart1(){ 
     amount ++;
+    counter++;
+
+
     document.getElementById("counter").innerHTML=" Du har lagt till " + counter + " av den bästa boosten ever! glhf!";
     alert('added to cart boosting'+ " "+amount);
   
@@ -11,21 +13,32 @@ function addToCart1(){
 
    if(amount==1){
     document.getElementById("korg").src="/images/rank1.png";
+    
+    
    }
    if(amount==2){
 
     document.getElementById("korg").src="/images/rank2.jpg";
    }
-   else if(amount>2){
+   
+   else if(amount>=2){
 
+        if(coachingCounter>=1&&counter>=1)
+   {
+
+    document.getElementById("garanterat").innerHTML=" Du har valt både coachning och boostning! Grandmaster är garanterat! grattis!";
+   
     document.getElementById("korg").src="/images/GM1.jpg";
+   }
+
    }
 
    
 }
 function addToCart2(){
-    coachingCounter++;
     amount ++;
+    coachingCounter++;
+    
     document.getElementById("coachingCounter").innerHTML= " Du har lagt till " + coachingCounter + " av den bästa coachningen ever! glhf!";
     alert('added to cart coaching'+ " "+amount);
   
@@ -33,14 +46,21 @@ function addToCart2(){
 
    if(amount==1){
     document.getElementById("korg").src="/images/rank1.png";
+    
    }
    if(amount==2){
 
     document.getElementById("korg").src="/images/rank2.jpg";
    }
-   else if(amount>2){
-
-    document.getElementById("korg").src="/images/GM1.jpg";
+   
+   else if(amount>=2){
+    if(coachingCounter>=1&&counter>=1)
+    {
+ 
+     document.getElementById("garanterat").innerHTML=" Du har valt både coachning och boostning! Grandmaster är garanterat! grattis!";
+    
+            document.getElementById("korg").src="/images/GM1.jpg";
+    }
    }
 
    
